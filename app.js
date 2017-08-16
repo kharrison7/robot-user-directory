f600379d3eb4fb88d452646b7f7dc9c95eef7051
Kevin Harrison <kharrison7@elon.edu>
2017-08-16 11:35:37 -0400

    This makes the dataSet console.log to the terminal.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
-
+const dataSet = require('./items.js');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -11,24 +11,19 @@ console.log("App is running");
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views')
+app.set('views', __dirname + '/views');
+
+  console.log(dataSet);
 
 //Listening on root
 app.get('/todo/', function (req, res) {
   console.log(req.additions); //This will print the array of values.
 
   // TODO write your code here
-
-  res.render('todo.mustache',  {
-
-                                }
+  res.render('todo.mustache',  {text1: "A Robot Directory"
+                               }
  );
 });
-
-let additions = [];
-app.use(bodyParser.urlencoded({ extended: true }));
-console.log("Additions Array: " + additions);
-app.use(bodyParser.json());
 
 app.listen(3000, function () {
   console.log('Successfully started express application!');
