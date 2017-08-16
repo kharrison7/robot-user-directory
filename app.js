df8125d39bc0a9894dab0bb7ed1114d0716fa16a
Kevin Harrison <kharrison7@elon.edu>
2017-08-16 13:49:45 -0400

    THis makes the list of robots appear.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
-const dataSet = require('./items.js');
-const filtered = require('./main.js');
+const data = require('./items.js');
+// const filtered = require('./main.js');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -12,24 +12,37 @@ console.log("App is running");
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views');
+// app.set('views', __dirname + '/views');
 
-  console.log(dataSet.users[1].id);
-  console.log(dataSet.users[1]);
+  console.log(data.users[1].id);
+  console.log(data.users[1]);
 
 //Listening on root
-app.get('/todo/', function (req, res) {
-  console.log(req.additions); //This will print the array of values.
 
-  // TODO write your code here
-  res.render('todo.mustache',  {text1: "A Robot Directory",
-                                 "unit": [{text2: dataSet.users[1].job,
-                                text3: dataSet.users[1].name,
-                                text4: dataSet.users[1].avatar,
-                                text5: dataSet.users[1].company
-                              }]}
- );
-});
+app.get('/users/', function(req, res){
+  res.render('users', data)});
+
+
+
+// app.get('/todo/', function (req, res) {
+//   console.log(req.additions); //This will print the array of values.
+//
+//   // TODO write your code here
+//   res.render('todo.mustache',  {text1: "A Robot Directory",
+//                                  "unit": [{text2: dataSet.users[1].job,
+//                                 text3: dataSet.users[1].name,
+//                                 text4: dataSet.users[1].avatar,
+//                                 text5: dataSet.users[1].company
+//                               },
+//                               {text2: dataSet.users[2].job,
+//                               text3: dataSet.users[2].name,
+//                               text4: dataSet.users[2].avatar,
+//                               text5: dataSet.users[2].company
+//                               }
+//
+//                             ]}
+//  );
+// });
 
 app.listen(3000, function () {
   console.log('Successfully started express application!');
--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 const dataSet = require('./items.js');
 
+let listOfUsers = [];
+let listOfJobs = [];
+let listOfCompanies = [];
+let listOfAvatars = [];
+
 function cleanOutput(){
-
 for( i=0; i<dataSet.users.length;i++){
-
-console.log("Hi");
-
-
+// console.log("Hi");
+listOfUsers[i] = dataSet.users[i].name;
+listOfJobs[i] = dataSet.users[i].job;
+listOfAvatars[i] = dataSet.users[i].avatar,
+listOfCompanies[i] = dataSet.users[i].company;
 }
-
-
-
+let totalArray = [listOfUsers, listOfJobs, listOfAvatars, listOfCompanies];
+console.log(totalArray[0]);
+console.log(listOfUsers);
+return totalArray;
 };
 
 
